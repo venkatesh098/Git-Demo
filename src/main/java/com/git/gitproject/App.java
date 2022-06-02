f0bd721207944841c39f6f3cc0285662440df6bf
venkat <kormanivenkatesh098@gmail.com>
2022-06-03 00:39:27 +0530

    buh 11 fixed

--- src/main/java/com/git/gitproject/App.java
+++ src/main/java/com/git/gitproject/App.java
@@ -6,9 +6,13 @@ package com.git.gitproject;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-    }
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//        
+//    }
+	
+	public String g() {
+		return "fd";
+	}
 }
