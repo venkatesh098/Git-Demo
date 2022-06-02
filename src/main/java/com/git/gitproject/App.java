79bda4be8321beefb642ca88e159904799ff131a
venkat <kormanivenkatesh098@gmail.com>
2022-06-02 23:32:15 +0530

    main program

---
+++ src/main/java/com/git/gitproject/App.java
@@ -0,0 +1,14 @@
+package com.git.gitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
